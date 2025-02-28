532f02148931db45f202f122f25b399ec51ba39e
RRahardjo88 <raissarahardjo@gmail.com>
2025-02-28 23:28:53 +1100

    script

--- GDS Lab2/Assets/Scripts/PowerUps/PowerUps.cs
+++ GDS Lab2/Assets/Scripts/PowerUps/PowerUps.cs
@@ -1,34 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-
 public class PowerUps : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 initialVelocity; 
+    private Vector2 initialVelocity;
+
     [SerializeField]
     private float reenableColliderAfter;
+
     private Rigidbody2D _rigidbody;
     private Collider2D _collider;
-}
-private void Start()
-{
-    _rigidbody= GetComponent<Rigidbody2D>(); 
-    _collider = GetComponent<Collider2D>(); 
-    _collider.enabled = false;
-    _rigidbody.velocity initialVelocity; 
-    StartCoroutine (routine: ReenableCollider());
-}
-private IEnumerator ReenableCollider()
-{
-    yield return new WaitForSeconds (_reenableColliderAfter);
-    _collider.enabled = true;
-}
-private void OnCollisionEnter2D (Collision2D other)
-{
-    var player = other.collider.GetComponent<Player>();
-    if (player != null)
+
+    void Start()
     {
-        Destroy (gameObject);
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+
+        _collider.enabled = false;
+        _rigidbody.linearVelocity = initialVelocity;
+
+        StartCoroutine(ReenableCollider());
+    }
+
+    IEnumerator ReenableCollider()
+    {
+        yield return new WaitForSeconds(reenableColliderAfter);
+        _collider.enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.collider.GetComponent<Mario>();
+        if (player != null)
+        {
+            Destroy(gameObject);
+        }
     }
 }
