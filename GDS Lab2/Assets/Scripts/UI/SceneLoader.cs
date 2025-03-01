19336c765ff47ebef63f1851aa9843e479978863
leila <justforgamingandmusicstuff@gmail.com>
2025-03-02 01:52:15 +1100

    editing loading screen

--- GDS Lab2/Assets/Scripts/UI/SceneLoader.cs
+++ GDS Lab2/Assets/Scripts/UI/SceneLoader.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Loading Screen");
+        Debug.Log("Game Loaded");
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Main Level");
