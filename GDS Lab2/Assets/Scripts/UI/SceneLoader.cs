58c14c048d89c6c813744737b881ce3196108a31
leila <justforgamingandmusicstuff@gmail.com>
2025-03-02 02:04:22 +1100

    start and loading scene functionality

--- GDS Lab2/Assets/Scripts/UI/SceneLoader.cs
+++ GDS Lab2/Assets/Scripts/UI/SceneLoader.cs
@@ -1,17 +1,34 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public float delayBeforeLoadingMainScene = 2.0f; 
+
+    void Start()
+    {
+        StartGame();
+    }
+
     public void LoadGame()
     {
+        Debug.Log("LoadGame method called");
         SceneManager.LoadScene("Loading Screen");
-        Debug.Log("Game Loaded");
+        Debug.Log("Loading Screen Loaded");
     }
 
     public void StartGame()
     {
+        Debug.Log("StartGame method called");
+        StartCoroutine(LoadMainSceneAfterDelay());
+    }
+
+    private IEnumerator LoadMainSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeLoadingMainScene);
+
         SceneManager.LoadScene("Main Level");
-        Debug.Log("Game Started");
+        Debug.Log("Main Level Loaded");
     }
 }
