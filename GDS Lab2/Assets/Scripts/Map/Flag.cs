d0d572090c00eb9c6f0f78730f210712e3e503cf
Resipate <105903976+Resipate@users.noreply.github.com>
2025-03-02 14:22:04 +1100

    finished flag

--- GDS Lab2/Assets/Scripts/Map/Flag.cs
+++ GDS Lab2/Assets/Scripts/Map/Flag.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
-    [SerializeField] private float collY;
+    [SerializeField] private float checkVal;
     [SerializeField] private bool flagTriggered;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject flagSheet;
@@ -12,7 +12,7 @@ public class Flag : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player")){
             flagTriggered = true;
-            collY = collision.transform.position.y;
+            checkVal = collision.transform.position.y;
             Destroy(collision.gameObject.GetComponent<MarioMovement>());
             stage = 0;
         }
@@ -23,9 +23,45 @@ public class Flag : MonoBehaviour
         if(flagTriggered){
             if(stage == 0){
                 //Set Sprite to climb
-                player.transform.position = new Vector2(this.transform.position.x - 0.4f, collY);
-                flagSheet.transform.position = new Vector2(this.transform.position.x - 0.75f, collY);
+                player.transform.position = new Vector2(this.transform.position.x - 0.4f, checkVal);
+                flagSheet.transform.position = new Vector2(this.transform.position.x - 0.75f, checkVal);
+                stage = 1;
             }
+            else if(stage == 1){
+                checkVal -= 7*Time.deltaTime;
+                if(checkVal <= 2){
+                    checkVal = 2;
+                    stage = 3;
+                }
+                player.transform.position = new Vector2(this.transform.position.x - 0.4f, checkVal);
+                flagSheet.transform.position = new Vector2(this.transform.position.x - 0.75f, checkVal);
+                if(stage == 3){ 
+                    player.transform.position = new Vector2(this.transform.position.x + 0.4f, 2);
+                    player.transform.localScale = new Vector2(-1, 1);
+                    player.gameObject.GetComponent<Rigidbody2D>().gravityScale = 10;
+                }
+            }
+            else if(stage == 3){
+                if(player.transform.position.y <= 1.1f){
+                    checkVal = 200;
+                    player.transform.position = new Vector2(200, 1);
+                    stage = 4;
+                    player.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                    player.transform.localScale = new Vector2(1, 1);
+                }
+            }
+            else if(stage == 4){
+                checkVal += 3*Time.deltaTime;
+                player.transform.position = new Vector2(checkVal, 1);
+                if(player.transform.position.x >= 205){
+                    stage = 5;
+                }
+            }
+            else if(stage == 5){
+                Destroy(player);
+                stage = 6;
+            }
+
         }
     }
 }
